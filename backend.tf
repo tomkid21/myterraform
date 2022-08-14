40f758e21c55bbab9a9e428fdaf27026d87d9e5e
Oluwaseun-Mensah <seunmichael21@yahoo.com>
2022-08-14 08:25:06 -0400

    newupdate

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraformstates31997"
-    key    = "state1"
-    region = "us-east-1"
-    profile= "Production1"
-    dynamodb_table = "terraform-lock"
-  }
-}
---
+++ terraformcloud.tf
@@ -0,0 +1,9 @@
+terraform {
+  cloud {
+    organization = "seunmensah"
+
+    workspaces {
+      name = "windows"
+    }
+  }
+}
