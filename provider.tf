9c35f8ef8d3d8c0b80c3026501c2002604009fca
Oluwaseun-Mensah <seunmichael21@yahoo.com>
2022-08-14 08:47:47 -0400

    removed the ec2

--- provider.tf
+++ provider.tf
@@ -9,10 +9,3 @@ resource "aws_instance" "ec2_example" {
     }
 }
 
-resource "aws_instance" "ec1_example" {
-    ami = "ami-090fa75af13c156b4"
-    instance_type = "t2.micro"
-    tags = {
-      Name = "EC2 Instance with remote state"
-    }
-}
