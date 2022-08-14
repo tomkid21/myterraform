87846d8f4bd624837461139bebc70fff31701f88
Oluwaseun-Mensah <seunmichael21@yahoo.com>
2022-08-14 08:33:28 -0400

    new ec2 instance

--- provider.tf
+++ provider.tf
@@ -18,3 +18,11 @@ resource "aws_instance" "ec2_example" {
       Name = "EC2 Instance with remote state"
     }
 }
+
+resource "aws_instance" "ec1_example" {
+    ami = "ami-090fa75af13c156b4"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "EC2 Instance with remote state"
+    }
+}
